18a7ae63e3f67897ea726b1891a4aa7d8f0ba1cf
momina-nz <momina.n494@gmail.com>
2025-01-28 02:22:55 +0500

    remove clerk auth

--- src/pages/PageNav.jsx
+++ src/pages/PageNav.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
 import { useDispatch, useSelector } from 'react-redux';
 import { toggleTheme } from '../utils/themeSlice';
 import { MdOutlineDarkMode } from "react-icons/md";
@@ -11,10 +10,14 @@ import Button from '../ui/Button';
 const PageNav = () => {
   const dispatch = useDispatch();
   const isDarkMode = useSelector((state) => state.theme.isDarkMode);
-  const [isOpen, setIsOpen] = useState(false); 
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleToggleMenu = () => {
     setIsOpen(!isOpen);
+  };
+
+  const handleRegisterClick = () => {
+    console.log("Register button clicked - functionality to be implemented");
   };
   
   return (
@@ -48,36 +51,16 @@ const PageNav = () => {
       {/* Navigation Links */}
       <ul
         className={`flex items-center p-3 font-semibold tracking-widest space-x-4 ${isOpen ? 'flex-col  absolute top-16 left-0 bg-teal-900 w-full mt-5 md:w-auto md:flex-row' : 'hidden md:flex'}`}>
-        {/* Auth Buttons */}
-        <SignedOut>
         <li className="navitems xs:my-1">
           <NavLink to="/">Home</NavLink>
         </li>
-          <li className="navitems xs:my-2">
-            <SignInButton>
-             REGISTER
-            </SignInButton>
-          </li>
-        </SignedOut>
-
-        <SignedIn> 
-        <li className="navitems xs:my-1">
-        <NavLink to="/app/carpoolDashboard">Carpools</NavLink>
-      </li>
-      <li className="navitems xs:my-1">
-        <NavLink to="/app/offerRide">Offer Ride</NavLink>
-      </li>
-      <li className="navitems xs:my-1">
-        <NavLink to="/app/findRide">Find Ride</NavLink>
-      </li>
-      <li className="xs:my-2">
-        <UserButton />
-      </li>
-        </SignedIn>
-
+        <li className="navitems xs:my-2">
+          <Button onClick={handleRegisterClick}>REGISTER</Button>
+        </li>
         <li className="mx-3 text-lg xs:my-1">
-            <Button type="modeBtn" onClick={() => dispatch(toggleTheme())}>{isDarkMode ? <CiLight /> : <MdOutlineDarkMode />}
-            </Button>
+          <Button type="modeBtn" onClick={() => dispatch(toggleTheme())}>
+            {isDarkMode ? <CiLight /> : <MdOutlineDarkMode />}
+          </Button>
         </li>
       </ul>
     </nav>
